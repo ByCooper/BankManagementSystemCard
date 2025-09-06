6314d93fb4e051280e1db2ac61017f96e1fc3437
ByCooper <kup.official@yahoo.com>
2025-09-06 15:26:34 +0300

    Projact_Maven

---
+++ src/main/java/ru/bankmanagementcardsystem/BankManagementCardSystem/BankManagementCardSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.bankmanagementcardsystem.BankManagementCardSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagementCardSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementCardSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bankmanagementcardsystem/BankManagementCardSystem/BankManagementCardSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bankmanagementcardsystem.BankManagementCardSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementCardSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
